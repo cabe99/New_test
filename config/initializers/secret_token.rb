eb9327e99a037b4e7359c8bd63925fb2cce2e7ac
cabe99 <cabe99@gmail.com>
2011-10-31 20:09:13 +0100

    initial comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testowisko::Application.config.secret_token = '993074ed683bda7429941f931215c77cefb54c8027bd09210d85319cf1561f9d74f9c656d0f7cad7fe2ae748d13875ad9f3454080d90fc58eb877ddecf614688'
